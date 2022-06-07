09cecd31f22eabd45310442634e6ef601420ca5c
manishkumar98 <36549424+manishkumar98@users.noreply.github.com>
2022-06-07 16:45:49 +0530

    Searcg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import "./styles.css";
+import { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+//import logo from "./logo.svg";
+import "./App.css";
+import Search from "./search";
+//import Announcer from "./announcer";
 
-export default function App() {
+const posts = [
+  { id: "1", name: "This first post is about React" },
+  { id: "2", name: "This next post is about Preact" },
+  { id: "3", name: "We have yet another React post!" },
+  { id: "4", name: "This is the fourth and final post" }
+];
+
+const filterPosts = (posts, query) => {
+  if (!query) {
+    return posts;
+  }
+
+  return posts.filter((post) => {
+    const postName = post.name.toLowerCase();
+    return postName.includes(query);
+  });
+};
+
+const App = () => {
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get("s");
+  const [searchQuery, setSearchQuery] = useState(query || "");
+  const filteredPosts = filterPosts(posts, searchQuery);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <ul>
+          {filteredPosts.map((post) => (
+            <li key={post.id}>{post.name}</li>
+          ))}
+        </ul>
+      </div>
+    </Router>
   );
-}
+};
+
+export default App;
